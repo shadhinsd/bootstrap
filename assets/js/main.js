6df6abc6a6cd952aa2e16ed3c5d2c74b8b37a671
shadhinsd <shadhinsd4477@gmail.com>
2023-09-20 19:02:39 +0600

    testimonial

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+$('.responsive').slick({
+    dots: true,
+    infinite: false,
+    speed: 1500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
